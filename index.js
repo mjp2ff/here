f74d710c3c531091a5d5ad85a68f75a54c0376c1
rbk2kb <rbk2kb@virginia.edu>
2014-04-12 03:33:10 -0400

    Added actual server for testing

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var pg = require('pg');
-
+var express = require('express');
+var app = express();
 pg.connect(process.env.DATABASE_URL, function(err, client, done) {
   if(err) {
     return console.error('error fetching client from pool', err);
   }
+});
+
+app.get('/', function(req, res) {
+  res.send('Hello Worlds!');
 });
