58f4e7f209dfdfd4675e0d34aa3ff05dd9a3acd1
Matt PB <mjp2ff@virginia.edu>
2014-04-12 05:25:29 -0400

    Changed client.py to have better generality.

--- testing/client.py
+++ testing/client.py
@@ -3,9 +3,9 @@ import json
 
 ex1 = """
     {
-        "sender":"appleforth the benign",
+        "sender":"Another person!",
         "url":"net.net",
-        "body":"Hellllllllllooooooooooooooasdf"
+        "body":"Hi sir"
     }
 """
 
@@ -14,12 +14,14 @@ ex2 = """
         "url":"net.net"
     }
 """
-# r = requests.post("http://glocale.herokuapp.com/newmessage", data=json, headers={"content-type":"application/json"})
-# print r.status_code
-
 # base = "http://localhost:5000"
 base = "http://glocale.herokuapp.com"
 
+# r = requests.post(base + "/newmessage", data=ex1, headers={"content-type":"application/json"})
 r = requests.get(base + "/getmessages", data=ex2, headers={"content-type":"application/json"})
+
 print r.status_code
-print json.dumps(r.json(), indent=4)
+try:
+	print json.dumps(r.json(), indent=4)
+except ValueError:
+	print "No json!"
