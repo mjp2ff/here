ffe8917b0e640a74d7b8cf7b544777814c711538
rbk2kb <rbk2kb@virginia.edu>
2014-04-12 04:00:00 -0400

    Adding back the database connection

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ var pg = require('pg');
 // var logfmt = require("logfmt");
 var express = require('express');
 var app = express();
-// pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-//   if(err) {
-//     return console.error('error fetching client from pool', err);
-//   }
-// });
+pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+  if(err) {
+    return console.error('error fetching client from pool', err);
+} else {
+    return console.log('We were succesful!');
+}
+});
 
 // app.use(logfmt.requestLogger());
 
