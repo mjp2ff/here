101eb4dbb065d387d7a46f200b93d9b0018e3055
Matt PB <mjp2ff@virginia.edu>
2014-04-12 19:07:31 -0400

    Cleaned up code in index.js, defaulted URLs to base page.

--- server/index.js
+++ server/index.js
@@ -15,19 +15,21 @@ var KEEP_TIME_SECONDS = 15*60;   // How many seconds to store messages for
 io.sockets.on('connection', function (socket) {
     // User joined page.
     socket.on('subscribe', function(data) {
-        socket.join(data.url);
-        console.log("Subscribed ", data.sender + " to ", data.url);
+        var chatURL = data.url.split('/')[2];
 
-        socket.broadcast.to(data.url).emit('userjoined', {
+        socket.join(chatURL);
+        console.log("Subscribed ", data.sender + " to ", chatURL);
+
+        socket.broadcast.to(chatURL).emit('userjoined', {
             user: data.sender, 
-            num_left: io.sockets.clients(data.url).length
+            num_left: io.sockets.clients(chatURL).length
         });
 
         pg.connect(process.env.DATABASE_URL, function(err, client, done) {
             if(err) {
                 return socket.emit('error', 'DB connection error');
             }
-            client.query("SELECT * FROM message WHERE url=$1", [data.url], function(err, result) {
+            client.query("SELECT * FROM message WHERE url=$1", [chatURL], function(err, result) {
                 if(err) {
                     return socket.emit('error', 'Messages not found');
                 }
@@ -38,38 +40,39 @@ io.sockets.on('connection', function (socket) {
 
     // User left page.
     socket.on('unsubscribe', function(data) {
-        console.log("Unsubscribed ", data.sender, " from ", data.url);
-        socket.broadcast.to(data.url).emit('userleft', {
+        var chatURL = data.url.split('/')[2];
+        
+        console.log("Unsubscribed ", data.sender, " from ", chatURL);
+        socket.broadcast.to(chatURL).emit('userleft', {
             user: data.sender, 
-            num_left: io.sockets.clients(data.url).length
+            num_left: io.sockets.clients(chatURL).length
         });
-        socket.leave(data.url);
+        socket.leave(chatURL);
     });
 
     // Someone sent a message!
     socket.on('sendmessage', function(data) {
+        var chatURL = data.url.split('/')[2];
+
         console.log("Server received message:", data, "from client");
-        socket.broadcast.to(data.url).emit('newmessage', data);
-        console.log("Now broadcasting message:", data, "to URL: ", data.url);
-        var sender = data.sender;
-        var url = data.url;
-        var body = data.body;
+        socket.broadcast.to(chatURL).emit('newmessage', data);
+        console.log("Now broadcasting message:", data, "to URL group: ", chatURL);
 
         pg.connect(process.env.DATABASE_URL, function(err, client, done) {
             if(err) {
                 return console.error('error fetching client from pool', err);
             }
 
-            client.query("INSERT INTO message(sender, url, body) VALUES ($1, $2, $3)", [sender, data.url, body], function(err, result) {
+            client.query("INSERT INTO message(sender, url, body) VALUES ($1, $2, $3)", [data.sender, chatURL, data.body], function(err, result) {
                 if(err) {
                     return console.error('error inserting message into database', err);
                 }
                 console.log('Successfully inserted new message!');
             });
 
-            deleteOldMessages(client, data.url);
+            deleteOldMessages(client, chatURL);
         });
-        socket.emit('numusers', io.sockets.clients(data.url).length);
+        socket.emit('numusers', io.sockets.clients(chatURL).length);
     });
 });
 
