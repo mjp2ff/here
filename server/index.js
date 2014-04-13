547162a5eb358a2800287dc9ab19fbd45612c3ed
Matt PB <mjp2ff@virginia.edu>
2014-04-12 21:06:34 -0400

    Replaced data.body with data when full data needed

--- server/index.js
+++ server/index.js
@@ -61,26 +61,23 @@ io.sockets.on('connection', function (socket) {
             var isGraffiti = (data.body.indexOf(":leave ") == 0);
 
             if (isGraffiti) {
-                var dataToSend = data.body.split(":leave ")[1];
-                client.query("INSERT INTO graffiti(sender, url, body) VALUES ($1, $2, $3)", [data.sender, chatURL, dataToSend], function(err, result) {
+                data.body = data.body.split(":leave ")[1];
+                client.query("INSERT INTO graffiti(sender, url, body) VALUES ($1, $2, $3)", [data.sender, chatURL, data.body], function(err, result) {
                     if(err) {
                         return console.error('error inserting graffiti into database', err);
                     }
                     console.log('Successfully inserted new graffiti!');
                 });
-                socket.broadcast.to(chatURL).emit('newmessage', dataToSend);
-                console.log("Now broadcasting message:", dataToSend, "to URL group:", chatURL);
             } else {
-                var dataToSend = data.body;
-                client.query("INSERT INTO message(sender, url, body) VALUES ($1, $2, $3)", [data.sender, chatURL, dataToSend], function(err, result) {
+                client.query("INSERT INTO message(sender, url, body) VALUES ($1, $2, $3)", [data.sender, chatURL, data.body], function(err, result) {
                     if(err) {
                         return console.error('error inserting message into database', err);
                     }
                     console.log('Successfully inserted new message!');
                 });
-                socket.broadcast.to(chatURL).emit('newmessage', dataToSend);
-                console.log("Now broadcasting message:", dataToSend, "to URL group:", chatURL);
             }
+            socket.broadcast.to(chatURL).emit('newmessage', data);
+            console.log("Now broadcasting message:", data, "to URL group:", chatURL);
 
             deleteOldMessages(client, chatURL, data.sender);
         });
